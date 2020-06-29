e975e131738cfb4cfb2e85c124596dfed8092bad
Ales401 <axel.cespedes@utec.edu.pe>
2020-06-29 16:38:10 -0500

    Simulation

--- ros_ksimulation/src/utils.py
+++ ros_ksimulation/src/utils.py
@@ -14,9 +14,8 @@ def rotation(omega, th):
 
     """
     skomega = skew(omega)
-    R = np.eye(3) + np.sin(th)*skomega + (1-np.cos(th))*skomega.dot(skomega)
+    R = np.eye(3) + np.sin(th) * skomega + (1 - np.cos(th)) * skomega.dot(skomega)
     return R
-
 
 
 def transform(omega, v, th):
@@ -26,13 +25,14 @@ def transform(omega, v, th):
     """
     T = np.eye(4)
     skomega = skew(omega)
-    p = th*np.eye(3) + (1-np.cos(th))*skomega + \
-        (th-np.sin(th))*skomega.dot(skomega)
+    p = th * np.eye(3) + (1 - np.cos(th)) * skomega + \
+        (th - np.sin(th)) * skomega.dot(skomega)
     p = np.dot(p, v)
     R = rotation(omega, th)
     T[0:3, 0:3] = R
     T[0:3, 3] = p
     return T
+
 
 def Trotx(angle):
     """
@@ -40,11 +40,12 @@ def Trotx(angle):
 
     """
     T = np.eye(4)
-    ca = np.cos(angle); sa = np.sin(angle)
+    ca = np.cos(angle)
+    sa = np.sin(angle)
     R = np.array([[1., 0., 0.],
                   [0., ca, -sa],
                   [0., sa, ca]])
-    T[0:3,0:3] = R
+    T[0:3, 0:3] = R
     return T
 
 
@@ -54,11 +55,12 @@ def Troty(angle):
 
     """
     T = np.eye(4)
-    ca = np.cos(angle); sa = np.sin(angle)
-    R = np.array([[ ca, 0., sa],
-                  [ 0., 1., 0.],
+    ca = np.cos(angle)
+    sa = np.sin(angle)
+    R = np.array([[ca, 0., sa],
+                  [0., 1., 0.],
                   [-sa, 0., ca]])
-    T[0:3,0:3] = R
+    T[0:3, 0:3] = R
     return T
 
 
@@ -68,12 +70,14 @@ def Trotz(angle):
 
     """
     T = np.eye(4)
-    ca = np.cos(angle); sa = np.sin(angle)
-    R = np.array([[ ca, -sa, 0.],
-                  [ sa,  ca, 0.],
-                  [ 0.,  0., 1.]])
-    T[0:3,0:3] = R
+    ca = np.cos(angle)
+    sa = np.sin(angle)
+    R = np.array([[ca, -sa, 0.],
+                  [sa, ca, 0.],
+                  [0., 0., 1.]])
+    T[0:3, 0:3] = R
     return T
+
 
 def Ttransl(d):
     """
@@ -81,27 +85,29 @@ def Ttransl(d):
 
     """
     T = np.eye(4)
-    T[0,3] = d[0]
-    T[1,3] = d[1]
-    T[2,3] = d[2]
+    T[0, 3] = d[0]
+    T[1, 3] = d[1]
+    T[2, 3] = d[2]
     return T
+
 
 def rotationFromQuat(q):
     """
     Rotation Matrix from a quaternion
 
     """
-    R = np.zeros([3,3])
-    R[0,0] = 2.0*(q[0]*q[0]+q[1]*q[1])-1.0
-    R[0,1] = 2.0*(q[1]*q[2]-q[0]*q[3])
-    R[0,2] = 2.0*(q[1]*q[3]+q[0]*q[2])
-    R[1,0] = 2.0*(q[1]*q[2]+q[0]*q[3])
-    R[1,1] = 2.0*(q[0]*q[0]+q[2]*q[2])-1.0
-    R[1,2] = 2.0*(q[2]*q[3]-q[0]*q[1])
-    R[2,0] = 2.0*(q[1]*q[3]-q[0]*q[2])
-    R[2,1] = 2.0*(q[2]*q[3]+q[0]*q[1])
-    R[2,2] = 2.0*(q[0]*q[0]+q[3]*q[3])-1.0
+    R = np.zeros([3, 3])
+    R[0, 0] = 2.0 * (q[0] * q[0] + q[1] * q[1]) - 1.0
+    R[0, 1] = 2.0 * (q[1] * q[2] - q[0] * q[3])
+    R[0, 2] = 2.0 * (q[1] * q[3] + q[0] * q[2])
+    R[1, 0] = 2.0 * (q[1] * q[2] + q[0] * q[3])
+    R[1, 1] = 2.0 * (q[0] * q[0] + q[2] * q[2]) - 1.0
+    R[1, 2] = 2.0 * (q[2] * q[3] - q[0] * q[1])
+    R[2, 0] = 2.0 * (q[1] * q[3] - q[0] * q[2])
+    R[2, 1] = 2.0 * (q[2] * q[3] + q[0] * q[1])
+    R[2, 2] = 2.0 * (q[0] * q[0] + q[3] * q[3]) - 1.0
     return R
+
 
 def Tmat(q):
     """
@@ -112,17 +118,16 @@ def Tmat(q):
 
     """
     T = np.array([
-        [-2.0*q[4],  2.0*q[3], -2.0*q[6],  2.0*q[5]],
-        [-2.0*q[5],  2.0*q[6],  2.0*q[3], -2.0*q[4]],
-        [-2.0*q[6], -2.0*q[5],  2.0*q[4],  2.0*q[3]]])
+        [-2.0 * q[4], 2.0 * q[3], -2.0 * q[6], 2.0 * q[5]],
+        [-2.0 * q[5], 2.0 * q[6], 2.0 * q[3], -2.0 * q[4]],
+        [-2.0 * q[6], -2.0 * q[5], 2.0 * q[4], 2.0 * q[3]]])
     return T
 
-def quaternionMult(q1,q2):
-    
-    qout = np.zeros(4)
-    qout[0] = -q1[1]*q2[1] - q1[2]*q2[2] - q1[3]*q2[3] + q1[0]*q2[0]
-    qout[1] =  q1[0]*q2[1] - q1[3]*q2[2] + q1[2]*q2[3] + q1[1]*q2[0]
-    qout[2] =  q1[3]*q2[1] + q1[0]*q2[2] - q1[1]*q2[3] + q1[2]*q2[0]
-    qout[3] = -q1[2]*q2[1] + q1[1]*q2[2] + q1[0]*q2[3] + q1[3]*q2[0]
-    return qout
+def quaternionMult(q1, q2):
 
+    qout = np.zeros(4)
+    qout[0] = -q1[1] * q2[1] - q1[2] * q2[2] - q1[3] * q2[3] + q1[0] * q2[0]
+    qout[1] = q1[0] * q2[1] - q1[3] * q2[2] + q1[2] * q2[3] + q1[1] * q2[0]
+    qout[2] = q1[3] * q2[1] + q1[0] * q2[2] - q1[1] * q2[3] + q1[2] * q2[0]
+    qout[3] = -q1[2] * q2[1] + q1[1] * q2[2] + q1[0] * q2[3] + q1[3] * q2[0]
+    return qout
