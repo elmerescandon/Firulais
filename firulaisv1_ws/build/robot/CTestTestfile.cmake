435b6e4754f28a028f52747512df4b3e6b607461
Raul Escandon <elmer.escandon@utec.edu.pe>
2020-07-11 02:17:20 -0500

    Mistake

--- firulaisv1_ws/build/robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raul/firulaisv1_ws/src/robot
-# Build directory: /home/raul/firulaisv1_ws/build/robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- firulaisv1_ws/build/robot_firulais/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raul/firulaisv1_ws/src/robot_firulais
-# Build directory: /home/raul/firulaisv1_ws/build/robot_firulais
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
