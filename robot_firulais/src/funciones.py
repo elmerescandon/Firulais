8db6f438812ebdfd2e5203447b4d6535e825c6fd
Raul Escandon <elmer.escandon@utec.edu.pe>
2020-07-18 21:38:48 -0500

    Differential Kinematics

--- robot_firulais/src/funciones.py
+++ robot_firulais/src/funciones.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+from copy import copy
 
-
-# ===============================
+# ==================================
 #  Funciones Numéricas Primarias
-# ===============================
+# ==================================
 
 
 def sdh(d, th, a, alpha):
@@ -76,6 +76,17 @@ def sTrotz(ang):
                     [0,0,0,1]])
     return Tz
 
+def crossproduct(a, b):
+    '''
+    Funcion numérica que retorna producto
+    cruz de los vectores a y b (ambos arrays)
+    '''
+    x = np.array([[a[1]*b[2] - a[2]*b[1]],
+                  [a[2]*b[0] - a[0]*b[2]],
+                  [a[0]*b[1] - a[1]*b[0]]])
+    return x
+
+
 # =================================
 #  Funciones Cinemática Directa
 # =================================
@@ -136,86 +147,147 @@ def fk_pata4_pos(q):
 	T4_B3 = T4_B0.dot(T4_03)
 	return T4_B3
 
+def fk_pata_pos(q,pata):
+    if pata == 1:
+    	T = fk_pata1_pos(q)
+    elif pata == 2: 
+    	T = fk_pata2_pos(q)
+    elif pata == 3: 
+    	T = fk_pata3_pos(q)
+    elif pata == 4: 
+    	T = fk_pata4_pos(q)
+    return T
+
+
 # ==============================================
-# Cinemática directa de cada pata considerando
-# el estaoo de posición y orientación (quaternion)
+# Cinemática directa de cada pata de cada transformación
 
 def fk_pata1(q):
 	# Pata 1
-	l1 = 0.125;l2=0.025;l3=0.103;l4=0.104;d1=0.075
+	l1 = 0.125;l2=0.025;l3=0.105;l4=0.104;d1=0.075
+	#l1 = 125;l2 = 25;l3 = 103;l4 = 104;d1 = 75
 	q11 = q[0];q12 = q[1];q13 = q[2];
 	T1_01 = sdh(l1, q11, 0, np.pi/2)
 	T1_12 = sdh(l2, q12, -l3, 0)
 	T1_23 = sdh(0, q13, -l4, 0)
 	T1_03 = T1_01.dot(T1_12).dot(T1_23)
-	R = T1_03[0:3,0:3]
-	quat_1 = rot2quaternion(R)
-	x  = np.array([[T1_03[0,3]],
-				   [T1_03[1,3]],
-				   [T1_03[2,3]],
-				   [quat_1[0,0]],
-				   [quat_1[1,0]],
-				   [quat_1[2,0]],
-				   [quat_1[3,0]]])
-	return x
+	T1_B0 = sTroty(-np.pi/2).dot(sTrotx(np.pi)).dot(sTrasl(0,-d1,0))
+	T1_B1 = T1_B0.dot(T1_01)
+	T1_B2 = T1_B0.dot(T1_01).dot(T1_12)
+	T1_B3 = T1_B0.dot(T1_03)
+	T = [T1_B0,T1_B1,T1_B2,T1_B3]
+	return T
 
 
 def fk_pata2(q):
 	# Pata 2
-	l1 = 0.125;l2=0.025;l3=0.103;l4=0.104;d1=0.075
+	l1 = 0.125;l2=0.025;l3=0.105;l4=0.104;d1=0.075
+	#l1 = 125;l2 = 25;l3 = 103;l4 = 104;d1 = 75
 	q21 = q[0];q22 = q[1];q23 = q[2];
 	T2_01 = sdh(l1, np.pi+q21, 0, np.pi/2)
 	T2_12 = sdh(l2, q22, l3, 0)
 	T2_23 = sdh(0, q23, l4, 0)
-	T2_03 = T2_01.dot(T2_12).dot(T2_23)
-	R = T2_03[0:3,0:3]
-	quat_2 = rot2quaternion(R)
-	x  = np.array([[T2_03[0,3]],
-				   [T2_03[1,3]],
-				   [T2_03[2,3]],
-				   [quat_2[0,0]],
-				   [quat_2[1,0]],
-				   [quat_2[2,0]],
-				   [quat_2[3,0]]])
-	return x
+	T2_03 = (T2_01.dot(T2_12)).dot(T2_23)
+	T2_B0 = sTroty(-np.pi/2).dot(sTrasl(0,d1,0))
+	T2_B1 = T2_B0.dot(T2_01)
+	T2_B2 = T2_B0.dot(T2_01).dot(T2_12)
+	T2_B3 = T2_B0.dot(T2_03)
+	T = [T2_B1,T2_B2,T2_B3]
+	return T
 
 def fk_pata3(q):
 	# Pata 3
-	l1 = 0.125;l2=0.025;l3=0.103;l4=0.104;d1=0.075
+	l1 = 0.125;l2=0.025;l3=0.105;l4=0.104;d1=0.075
+	#l1 = 125;l2 = 25;l3 = 103;l4 = 104;d1 = 75
 	q31 = q[0];q32 = q[1];q33 = q[2];
 	T3_01 = sdh(l1, np.pi+q31, 0, np.pi/2)
 	T3_12 = sdh(l2, q32, l3, 0)
 	T3_23 = sdh(0, q33, l4, 0)
 	T3_03 = T3_01.dot(T3_12).dot(T3_23)
-	R = T3_03[0:3,0:3]
-	quat_3 = rot2quaternion(R)
-	x  = np.array([[T3_03[0,3]],
-				   [T3_03[1,3]],
-				   [T3_03[2,3]],
-				   [quat_3[0,0]],
-				   [quat_3[1,0]],
-				   [quat_3[2,0]],
-				   [quat_3[3,0]]])
-	return x
+	T3_B0 = sTroty(-np.pi/2).dot(sTrotx(np.pi)).dot(sTrasl(0,d1,0))
+	T3_B1 = T3_B0.dot(T3_01)
+	T3_B2 = T3_B0.dot(T3_01).dot(T3_12)
+	T3_B3 = T3_B0.dot(T3_03)
+	T = [T3_B1,T3_B2,T3_B3]	
+	return T 
 
 
 def fk_pata4(q):
-	l1 = 0.125;l2=0.025;l3=0.103;l4=0.104;d1=0.075
+	# Pata 4
+	l1 = 0.125;l2=0.025;l3=0.105;l4=0.104;d1=0.075	
 	q41 = q[0];q42 = q[1];q43 = q[2];
 	T4_01 = sdh(l1, q41, 0, np.pi/2)
 	T4_12 = sdh(l2, q42, -l3, 0)
 	T4_23 = sdh(0, q43, -l4, 0)
 	T4_03 = T4_01.dot(T4_12).dot(T4_23)
-	R = T4_03[0:3,0:3]
-	quat_4 = rot2quaternion(R)
-	x  = np.array([[T4_03[0,3]],
-				   [T4_03[1,3]],
-				   [T4_03[2,3]],
-				   [quat_4[0,0]],
-				   [quat_4[1,0]],
-				   [quat_4[2,0]],
-				   [quat_4[3,0]]])
-	return x
+	T4_B0 = sTroty(-np.pi/2).dot(sTrasl(0,-d1,0))
+	T4_B1 = T4_B0.dot(T4_01)
+	T4_B2 = T4_B0.dot(T4_01).dot(T4_12)
+	T4_B3 = T4_B0.dot(T4_03)
+	T = [T4_B1,T4_B2,T4_B3]
+	return T
 
 
 
+# ===============================================
+#  Funciones Cinemática Diferencial
+# ===============================================
+
+def jacob_g(q,pata):
+    '''
+    Función que realiza el Jacobiano geométrico dado los parámetros
+    Denavit-Hatenberg. Retorna la matriz jacobiana considerando 
+    la velocidad lineal y angular del sistema
+    '''
+    if pata == 1:
+    	T_ref = fk_pata1(q)
+    elif pata == 2: 
+    	T_ref = fk_pata2(q)
+    elif pata == 3: 
+    	T_ref = fk_pata3(q)
+    elif pata == 4: 
+    	T_ref = fk_pata4(q)
+    d1=0.075	
+    p0 = T_ref[0][0:3,3]
+    z0 = T_ref[0][0:3,2]
+    T_ref = T_ref[1:4]
+    J = [];
+    for n in range(len(T_ref)):
+        if (n == 0):
+            z = z0
+            p = p0
+        else:
+            z = T_ref[n-1][0:3, 2]
+            p = T_ref[n-1][0:3, 3]
+
+        # Todas son articulaciones de revolución
+        Jv = crossproduct(z, T_ref[len(T_ref)-1][0:3, 3] - p)
+        Jw = np.array([[z[0]],[z[1]],[z[2]]])
+        J1 = np.vstack((Jv, Jw))
+        if n == 0: 
+        	J = J1
+        else:
+        	J = np.hstack((J, J1))
+    return J
+
+
+def jacob_a_pos(q,pata,delta=0.0001):
+    '''
+    Función que realiza el Jacobiano analítico de la posición
+    de un pata con respecto a la base del robot
+    '''
+    # Crear una matriz 3x3
+    J = np.zeros((3,3))
+    # Transformacion homogenea inicial (usando q)
+    T = fk_pata_pos(q,pata)
+    # Iteracion para la derivada de cada columna
+    for i in xrange(3):
+        # Copiar la configuracion articular inicial
+        dq = copy(q)
+        # Incrementar la articulacion i-esima usando un delta
+        dq[i]=dq[i]+delta
+        # Transformacion homogenea luego del incremento (q+delta)
+        dT=fk_pata_pos(dq,pata)
+        # Aproximacion del Jacobiano de posicion usando diferencias finitas
+        J[:,[i]]=(dT[:3,[3]]-T[:3,[3]])/delta
+    return J
